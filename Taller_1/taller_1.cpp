6b88fc9820eb165436995544e8400522c93d9a88
Briam Daniel Agudelo Mogollón <bridamo.98@gmail.com>
2018-07-28 16:05:22 -0500

    corregido sin cs

--- Taller_1/taller_1.cpp
+++ Taller_1/taller_1.cpp
@@ -10,21 +10,17 @@ double evaluarFuncion(double x)//FUNCION PARA EVALUAR LA FUNCION DESCRITA EN EL 
 }
 double taller(vector<double> limites, double particiones, double *error,double * recursiones, double xrAnterior, double* errorMasUno)
 {
-
     *recursiones=(*recursiones)+1;//SUMAR EN 1 EL NUMERO DE ITERACIONES(REECURSIONES)
     vector<double> Particiones ;//CONTENEDOR DE LAS NUEVAS PARTICIONES
     double seccion= (fabs((limites[0])-(limites[1]) )/particiones);//TAMAÑO DE CADA NUEVA PARTICION
     cout<<"seccion "<<seccion<<endl;
     double errorPorcentual ;//VARIABLE QUE TENDRÁ EL ERROR PORCENTUAL DE LA ITERACIÓN ACTUAL
-   
+    double xr=(limites[0] + limites[1])/2.00000;
     Particiones.push_back(limites[0]);//EL PRIMER LIMITE DEL CONTENEDOR DE PARTICIONES ES IGUAL AL LIMITE INFERIOR ESPECIFICADO PARA ESTA ITERACION
-    
     for (int i=0; i<particiones; i++)//CALCULAR LOS NUEVOS PUNTOS DEL CONTENEDOR DE PARTICIONES
     {
         Particiones .push_back(Particiones[i]+seccion);//--------------------------------------------
     }
-    
-
     vector <double>:: iterator iterador = Particiones.begin();//IMPRIMIR POR CONSOLA LAS NUEVAS PARTICIONES PARA UN SEGUIMIENTO DEL PROCESO
     for(; Particiones.end()!=iterador; iterador++)//-------------------------------------------------
     {
@@ -34,28 +30,17 @@ double taller(vector<double> limites, double particiones, double *error,double *
     //--------------------esto lo puso briam------------------------------------------
     if(xrAnterior!=0.00000)
     {
-        errorPorcentual = ((limites[0] + limites[1])/2)*100.00000;
-        cout<<"ERROR PORCENTUAL ---"<<setprecision(5)<<errorPorcentual<<" con "<<seccion<<" "<<xrAnterior<<"--"<<seccion + xrAnterior<<endl; 
+        errorPorcentual = fabs((xr+xrAnterior/xr)*100.00000);
+        cout<<"ERROR PORCENTUAL ---"<<setprecision(5)<<errorPorcentual<<endl; 
     }
-    //-----------------------------------------------------------------------
     /*
 
     CASO BASE DE LA FUNCIÓN RECURSIVA
-    
 
     */
     for(int i=0; i<Particiones.size(); i++)//-----------------------------------------------------
     {
-        double lol=evaluarFuncion(Particiones[i]);//EVALULAR LA FUNCIÓN EN CADA PUNTO DE LAS PARTICIONES
-
-        /*if(lol<0)//ESTO LO COMENTÓ BRIAM
-        {
-            lol*=-1;
-        }*/
-
-
-
-        
+        double lol=evaluarFuncion(Particiones[i]);//EVALULAR LA FUNCIÓN EN CADA PUNTO DE LAS PARTICIONES        
         if(lol<=0.000001 && lol>=-0.000001)//LO QUE VA DESPUES DE && LO AGREGÓ BRIAM, VALIDANDO SI EL VALOR DE LA FUNCIÓN YA SE ACERCÓ A CERO LO SUFICIENTE
         {
             *errorMasUno=errorPorcentual;//AL SER LA ULTIMA ITERACION(IT) EL ERROR PORCENTUAL DE LA SIGUIENTE IT ES EL ERROR DE LA ACTUAL IT(NO EXISTE IT SIGUIENTE)
@@ -81,28 +66,17 @@ double taller(vector<double> limites, double particiones, double *error,double *
             vector<double> aux3;//SE CREA UN CONTENEDOR QUE GUARDARA LOS NUEVOS LIMITES DE LA SIGUIENTE PARTICIÓN
             aux3.push_back(Particiones[i-1]);//SE LES DA LOS VALORES A LOS LIMITES INFERIOR Y SUPERIOR OBTENIDOS ANTERIORMENTE
             aux3.push_back(Particiones[i]);//------------------------------------------------------------------------------
-
             float recActual=*recursiones;//SE CREA UNA COPIA DEL NUMERO DE LAS RECURSIONES LLEVADAS HASTA EL MOMENTO
-            xrAnterior=seccion;//ESTO LO AGREGO BRIAM
+            xrAnterior=xr;//ESTO LO AGREGO BRIAM
             double aux4 =taller(aux3,particiones, error, recursiones,xrAnterior,errorMasUno);//SE REALIZA EL LLAMADO RECURSIVO
-            //------------------esto lo agrego Briam--------------------------------------------------
-
             if(*recursiones!=1)
             {
                 double k=(*errorMasUno)/errorPorcentual;
                 cout<<" La convergencia para la iteracion "<<recActual<<" es "<<setprecision(6)<<k<<" con error Ei="<<errorPorcentual<<" y "<<" E(i+1) "<<*errorMasUno<<endl;
                 *errorMasUno= errorPorcentual;
             }
-             /*if(xrAnterior!=0.00000)
-             {
-            errorPorcentual = ((seccion + xrAnterior)/seccion)*100.00000; 
-             }
-            xrAnterior=seccion;*/
-
-           
             return aux4;
         }
-
     }
 }
 int main()
@@ -124,5 +98,5 @@ int main()
     *error=0;
     *errorMasUno=0.0000000;
     cout<<"---4 secciones---"<< endl<<"Respuesta :"<<taller(limites, cantParticiones,error, recursiones,0.00000,errorMasUno)<<endl;
-     cout<<"Error: "<<*error<<endl<<"# de recursiones: "<< *recursiones<<endl;
+    cout<<"Error: "<<*error<<endl<<"# de recursiones: "<< *recursiones<<endl;
 }
